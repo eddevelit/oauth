83121dce5339a30202acdf8607c46d8ba6f3e97f
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-27 12:31:46 -0600

    Agregando geters y seters a modelo Cuenta

--- src/main/java/com/team3/oauth/model/Cuenta.java
+++ src/main/java/com/team3/oauth/model/Cuenta.java
@@ -2,8 +2,31 @@ package com.team3.oauth.model;
 
 public class Cuenta {
 	public int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getSaldo() {
+		return Saldo;
+	}
+	public void setSaldo(double saldo) {
+		Saldo = saldo;
+	}
+	public String getEjecutivo() {
+		return Ejecutivo;
+	}
+	public void setEjecutivo(String ejecutivo) {
+		Ejecutivo = ejecutivo;
+	}
+	public int getId_cliente() {
+		return id_cliente;
+	}
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
 	public double Saldo;
 	public String Ejecutivo;
 	public int id_cliente;
-
 }
