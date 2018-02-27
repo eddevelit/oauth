f751034c24a787e0b206511dda72a0427c6e0a51
Eduardo Ocampo <edd.develit@gmail.com>
2018-02-27 12:23:30 -0600

    Agregando carpeta de model con Cliente.java y Cuenta.java

---
+++ src/main/java/com/team3/oauth/model/Cliente.java
@@ -0,0 +1,40 @@
+package com.team3.oauth.model;
+
+public class Cliente {
+	private int id;
+	private String Nombre;
+	private String Direccion;
+	private String Correo;
+	private String Telefono;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getDireccion() {
+		return Direccion;
+	}
+	public void setDireccion(String direccion) {
+		Direccion = direccion;
+	}
+	public String getCorreo() {
+		return Correo;
+	}
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
+	public String getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(String telefono) {
+		Telefono = telefono;
+	}
+}
---
+++ src/main/java/com/team3/oauth/model/Cuenta.java
@@ -0,0 +1,9 @@
+package com.team3.oauth.model;
+
+public class Cuenta {
+	public int id;
+	public double Saldo;
+	public String Ejecutivo;
+	public int id_cliente;
+
+}
